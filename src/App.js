61165841e179ed71471f2922c0f1c32902ddbb39
Gabriel Wilkins <wilkigab001@gmail.com>
2022-12-01 14:51:43 -0700

    Added start to header and home:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import AboutMe from './Components/AboutMe/AboutMe'
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Portfolio from './Components/Portfolio/Portfolio';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-
+      <Header/>
+      <Home/>
+      <AboutMe/>
+      <Portfolio/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={styles.allHeader}>
+      <div className={styles.headerTitle}> Gabriel Wilkins </div>
+      <div className={styles.listItems}>
+        <li>
+          <a href="#">About me</a>
+        </li>
+        <li>
+          <a href="#">Portfolio</a>
+        </li>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
